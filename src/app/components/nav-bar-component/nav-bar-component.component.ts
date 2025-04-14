09b8443a775b9df67394f6f7c47cb8df96a7a415
ahmedaljendy <elgendya166@gmail.com>
2025-04-14 15:14:19 +0200

    feat: add navigation bar component and integrate with app
    - Added `NavBarComponentComponent` for navigation.
    - Updated `app.component.html` to include the new navigation bar.
    - Modified `app.component.ts` to import and declare the new component.
    - Created HTML and CSS files for the navigation bar component.
    - Added unit tests for the navigation bar component.
    - Updated `package.json` to include Bootstrap and Popper.js for styling.
    - Added Angular localization support in main.ts and TypeScript configuration files.

---
+++ src/app/components/nav-bar-component/nav-bar-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-component',
+  imports: [],
+  templateUrl: './nav-bar-component.component.html',
+  styleUrl: './nav-bar-component.component.css',
+})
+export class NavBarComponentComponent {
+  imageUrl = 'https://cdn-icons-png.flaticon.com/128/679/679922.png';
+}
