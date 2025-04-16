6a6242519f01e8e4bda5ed69720b12eba24a1e8a
ahmedaljendy <elgendya166@gmail.com>
2025-04-16 23:19:08 +0200

    feat: add products component and integrate routing for products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductsComponentComponent } from './products-component/products-component.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'home' },
   { path: 'about', component: AboutComponent, title: 'about' },
   { path: 'contact', component: ContactComponent, title: 'contact' },
+  {
+    path: 'products',
+    component: ProductsComponentComponent,
+    title: 'products',
+  },
   { path: '**', component: NotFoundComponent, title: 'Not Found' },
 ];
